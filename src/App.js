6a32dc0611c51d6cba25b50cee7c26f73e376025
yohannes2025 <johannes.m.tekle@gmail.com>
2025-03-19 19:47:43 +0300

    Routing in React

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signin" render={() => <SignInForm />} /> 
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
